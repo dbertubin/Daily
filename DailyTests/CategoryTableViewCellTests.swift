3ba6eb5fd2403476e7609d204574a5e38ccb9493
Derek <derek@teeps.org>
2018-10-16 19:47:06 -0400

    Add test for CategoryTableViewCell

--- DailyTests/CategoryTableViewCellTests.swift
+++ DailyTests/CategoryTableViewCellTests.swift
@@ -13,6 +13,27 @@ import UIKit
 
 class CategoryTableViewCellTests: XCTestCase {
 
-    let category = Categor
+    let category = QuoteCategory(key: "test", value: "Test Value")
+
+    var categoriesTableViewController: CategoriesTableViewController? = nil
     
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        categoriesTableViewController = storyboard.instantiateViewController(withIdentifier: "CategoriesTableViewController") as? CategoriesTableViewController
+        categoriesTableViewController?.categories = [category]
+        categoriesTableViewController?.loadView() 
+        categoriesTableViewController?.tableView.reloadData()
+    }
+    
+    override func tearDown() {
+        categoriesTableViewController = nil
+    }
+    
+    func testConfigureCategoryTableViewCell() {
+        let indexPath = IndexPath(row: 0, section: 0)
+        let category2 = QuoteCategory(key: "test", value: "Test Value 2")
+        let cell = categoriesTableViewController?.tableView.cellForRow(at: indexPath) as! CategoryTableViewCell
+        cell.configure(with: category2)
+        XCTAssertEqual( cell.titleLabel.text, category2.value)
+    }
 }
